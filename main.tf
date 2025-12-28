650fc108560a04efe371bc99d46aa3a8a8a3005d
Muthu <muthuppandiac@gmail.com>
2025-12-29 02:36:08 +0530

    myy

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "aws_vpc" "main" {
 }
     resource "aws_subnet" "main_subnet" {
         vpc_id            = aws_vpc.main.id
-        cidr_block        = "10.0.0.0/28"
+        cidr_block        = "10.0.0.0/25"
         availability_zone = "us-east-1a"
         tags = {
-            Name = "main_subnes"
+            Name = "main_subnet"
             created_on = timestamp()
             created_by = "terraform"
         }
