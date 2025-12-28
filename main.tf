485e9404a22429cd122460eeec9768ee13f0b045
Muthu <muthuppandiac@gmail.com>
2025-12-29 02:53:33 +0530

    myy

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 }
     resource "aws_subnet" "main_subnet" {
         vpc_id            = aws_vpc.main.id
-        cidr_block        = "10.0.0.0/27"
+        cidr_block        = "10.0.0.0/28"
         availability_zone = "us-east-1a"
         tags = {
             Name = "main_subnet"
