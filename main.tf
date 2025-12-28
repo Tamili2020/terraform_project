3b83fbf2f26da01ee41dbbd0cd213d91b357be06
Muthu <muthuppandiac@gmail.com>
2025-12-29 02:17:53 +0530

    my

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "main_vpc"
+        created_on = timestamp()
+        created_by = "terraform"
+    }
+}
+    resource "aws_subnet" "main_subnet" {
+        vpc_id            = aws_vpc.main.id
+        cidr_block        = "10.0.0.0/28"
+        availability_zone = "us-east-1a"
+        tags = {
+            Name = "main_subnet"
+            created_on = timestamp()
+            created_by = "terraform"
+        }
+    
+  
+}
