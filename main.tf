2d94de6df0cf45d11c0a18afae4966b6fe631057
Muthu <muthuppandiac@gmail.com>
2026-04-29 07:47:58 +0530

    VPC code updated

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ provider "aws" {
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/24"
     tags = {
-        Name = "main_vpc"
+        Name = "main_vpc_test"
         created_on = timestamp()
-        created_by = "terraform"
+        created_by = "terraform_user_muthu"
     }
 }
     resource "aws_subnet" "main_subnet" {
         vpc_id            = aws_vpc.main.id
-        cidr_block        = "10.0.0.0/28"
+        cidr_block        = "10.0.0.0/27"
         availability_zone = "us-east-1a"
         tags = {
             Name = "main_subnet1"
             created_on = timestamp()
-            created_by = "terraform-user_23445"
+            created_by = "terraform-user_muthu"
         }
     
   
